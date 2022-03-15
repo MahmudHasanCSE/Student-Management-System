a34268cc735388fa15b8c32822f089a5753e7dcd
zero-fighter <rauaha@gmail.com>
2022-03-15 13:33:19 +0600

    pd 05

--- resources/views/admin/teacher/edit.blade.php
+++ resources/views/admin/teacher/edit.blade.php
@@ -6,44 +6,45 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title mb-4">Add Teacher Form</h4>
+                    <h4 class="card-title mb-4">Edit Teacher Form</h4>
                     <p class="text-center text-success">{{Session::get('message')}}</p>
-                    <form action="{{route('new-teacher')}}" method="post" enctype="multipart/form-data">
+                    <form action="{{route('update-teacher', ['id' => $teacher->id])}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group row mb-4">
                             <label for="horizontal-fullname-input" class="col-sm-3 col-form-label">Full name</label>
                             <div class="col-sm-9">
-                                <input type="text" name="name" class="form-control" id="horizontal-fullname-input">
+                                <input type="text" value="{{$teacher->name}}" name="name" class="form-control" id="horizontal-fullname-input">
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label for="horizontal-email-input" class="col-sm-3 col-form-label">Email</label>
                             <div class="col-sm-9">
-                                <input type="email" name="email" class="form-control" id="horizontal-email-input">
+                                <input type="email" value="{{$teacher->email}}" name="email" class="form-control" id="horizontal-email-input">
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label for="horizontal-mobile-input" class="col-sm-3 col-form-label">Mobile</label>
                             <div class="col-sm-9">
-                                <input type="number" name="mobile" class="form-control" id="horizontal-mobile-input">
+                                <input type="number" value="{{$teacher->mobile}}" name="mobile" class="form-control" id="horizontal-mobile-input">
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label for="horizontal-address-input" class="col-sm-3 col-form-label">Address</label>
                             <div class="col-sm-9">
-                                <textarea class="form-control" name="address" id="horizontal-address-input"></textarea>
+                                <textarea class="form-control" name="address" id="horizontal-address-input">{{$teacher->address}}</textarea>
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label for="horizontal-firstname-input" class="col-sm-3 col-form-label">Image</label>
                             <div class="col-sm-9">
                                 <input type="file" name="image" class="form-control-file" accept="image/*">
+                                <img src="{{asset($teacher->image)}}" alt="" height="100" width="150"/>
                             </div>
                         </div>
 {{--                        <div class="form-group row mb-4">--}}
 {{--                            <label for="horizontal-firstname-input" class="col-sm-3 col-form-label">Status</label>--}}
 {{--                            <div class="col-sm-9">--}}
-{{--                                <input type="number" name="status" class="form-control" id="horizontal-firstname-input">--}}
+{{--                                <input type="number" value="{{$teacher->code}}" name="status" class="form-control" id="horizontal-firstname-input">--}}
 {{--                            </div>--}}
 {{--                        </div>--}}
 
@@ -51,7 +52,7 @@
                             <div class="col-sm-9">
 
                                 <div>
-                                    <button type="submit" class="btn btn-primary w-md">Create New Teacher</button>
+                                    <button type="submit" class="btn btn-primary w-md">Update Teacher Information</button>
                                 </div>
                             </div>
                         </div>
